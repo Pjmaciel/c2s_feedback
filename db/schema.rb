ce8f922d3266ab9d421932b212e5c0358d33c15a
Pjmaciel <pjmaciel_4@hotmail.com>
2025-02-07 18:02:50 -0300

    chore(db): update schema after recent migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_07_025413) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_07_203302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attendant_profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "registration_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_attendant_profiles_on_user_id"
+  end
+
+  create_table "client_profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "cpf"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_client_profiles_on_user_id"
+  end
+
+  create_table "manager_profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "registration_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_manager_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +46,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_07_025413) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "attendant_profiles", "users"
+  add_foreign_key "client_profiles", "users"
+  add_foreign_key "manager_profiles", "users"
 end
