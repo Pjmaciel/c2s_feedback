b777e63e553bf8de2104b76d67397de7f5e1d810
Pjmaciel <pjmaciel_4@hotmail.com>
2025-02-07 00:46:55 -0300

    feat(auth): Implements registration, login and users logot

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require redoc.min
+import Rails from "@rails/ujs";
+Rails.start();
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get construction" do
+  test "should get home" do
     get pages_construction_url
     assert_response :success
   end
