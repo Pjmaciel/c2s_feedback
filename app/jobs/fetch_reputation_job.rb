7866e0dfa28816550627fe65e4615b4a5902e8f4
Pjmaciel <pjmaciel_4@hotmail.com>
2025-02-10 01:30:17 -0300

    perf(fetch_reputation_job): optimize Redis caching for reputation lookups

--- app/jobs/fetch_reputation_job.rb
+++ app/jobs/fetch_reputation_job.rb
@@ -1,29 +1,20 @@
-class FetchReputationJob < ApplicationJob
-  queue_as :default
+class FetchReputationJob
+  include Sidekiq::Job
 
   def perform(company_name, user_id)
-    Rails.logger.info "Executando FetchReputationJob para #{company_name}, Usuário: #{user_id}"
+    begin
+      company_info = ReclameAqui.fetch_company_info(company_name)
+      company_info.merge!(last_updated: Time.current.to_s)
 
-    company_info = ReclameAqui.fetch_company_info(company_name)
-
-    Rails.logger.info "Resultado do scraping: #{company_info.inspect}"
-
-    if company_info[:error]
-      Rails.logger.error "Erro ao buscar dados para #{company_name}"
-      ActionCable.server.broadcast("reputation_#{user_id}", { status: "Erro ao buscar dados." })
-    else
-      # Salvar no Redis
-      redis_key = "reclame_aqui:#{user_id}:#{company_name}"
-      Sidekiq.redis { |conn| conn.setex(redis_key, 24.hours.to_i, company_info.to_json) }
-
-      # Enviar para o WebSocket
-      Rails.logger.info "Enviando dados para WebSocket..."
-      ActionCable.server.broadcast("reputation_#{user_id}", {
-        company_name: company_info[:company_name],
-        solution_rate: company_info[:reviews_count],
-        reputation_score: company_info[:rating],
-        status: "Atualizado"
-      })
+      Sidekiq.redis do |conn|
+        conn.setex(
+          "reclame_aqui:#{user_id}:#{company_name}",
+          24.hours.to_i,
+          company_info.to_json
+        )
+      end
+    rescue => e
+      Rails.logger.error("Erro no FetchReputationJob: #{e.message}")
     end
   end
 end
